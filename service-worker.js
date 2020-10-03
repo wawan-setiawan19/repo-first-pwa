c98db6246f8c7b946f12198ef130ab027a622fa4
onepro19 <w.settiawan@gmail.com>
2020-10-03 20:42:40 +0700

    add configuration for caching

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "firstpwa-v2";
+const urlsToCache=[
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "js/nav.js"
+];
+
+// simpan hasil cache
+self.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache)=>{
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// menggunakan hasil cache
+self.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches
+            .match(event.request,   {cacheName:CACHE_NAME})
+            .then((response)=>{
+                if(response){
+                    console.log("ServiceWorker : Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ", event.request.url
+                );
+                return fetch(event.request);
+            })
+    );
+});
+
+// menghapus cache lama
+self.addEventListener("activate", (event)=>{
+    event.waitUntil(
+        caches.keys().then((cachesNames)=>{
+            return Promise.all(
+                cachesNames.map((cacheName)=>{
+                    if(cacheName != CACHE_NAME){
+                        console.log(`ServiceWorker: cache${cacheName} dihapus`);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
